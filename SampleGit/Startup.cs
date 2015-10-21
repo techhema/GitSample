7f8daf70f7586a714b460861caca454489f4d3ce
techhema <hema_koduri@yahoo.co.in>
2015-10-21 18:11:04 +0530

    First Init

---
+++ SampleGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleGit.Startup))]
+namespace SampleGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
